a4b5018761f574c7ae4d6747005408659fa4fa35
Christopher Romberg <apfeloxid@gmail.com>
2020-06-29 03:49:16 +0200

    Added legend; adjusted text; colored tiles of the same category with the same color

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const urlData = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
-const h = 600;
-const w = 1200;
+const h = 650;
+const w = 1400;
 const paddingBetweenCategory = 1;
 const paddingOutside = 10;
-
-console.log("hey");
 
 window.onload = function() {
 
@@ -13,23 +11,55 @@ window.onload = function() {
         .attr("height", h)
         .attr("width", w);
 
-    const tooltip = d3.select("#tooltip");
+    const legend = d3.select("#svg-container")
+        .append("svg")
+        .attr("id", "legend")
+        .attr("height", h)
+        .attr("width", 80);
 
+    const tooltip = d3.select("#tooltip");
+    
 
     d3.json(urlData).then(function(data) {
         console.log(data);
 
-        let root = d3.hierarchy(data).sum(function(d) {return d.value});
+        let categories = data.children.map(child => child.name);
+
+        let colors = [];
+        for (let i = 0; i < categories.length; i++) {
+            colors.push(d3.interpolateRainbow(i / categories.length))
+        }
+        colors = shuffle(colors);
+
+        console.log(colors);
+        console.log(categories);
+
+
+        let colorScale = d3.scaleOrdinal()
+                .domain(categories)
+                .range(colors);
+
+        let root = d3.hierarchy(data).sum(function(d) {return d.value}).sort(function(a, b) { return b.height - a.height || b.value - a.value; });
+
+
 
         d3.treemap()
             .size([w, h])
             .padding(paddingBetweenCategory)
             (root);
 
-        svg.selectAll("rect")
+        let containers = svg.selectAll("svg")
             .data(root.leaves())
             .enter()
-            .append("rect")
+            .append("g")
+            .attr("x", d => d.x0)
+            .attr("y", d => d.y0)
+            .attr("width", d => d.x1 - d.x0)
+            .attr("height", d => d.y1 - d.y0)
+
+
+
+        containers.append("rect")
             .attr("class", "tile")
             .attr("data-name", d => d.data.name)
             .attr("data-category", d => d.data.category)
@@ -38,7 +68,7 @@ window.onload = function() {
             .attr("y", d => d.y0)
             .attr("width", d => d.x1 - d.x0)
             .attr("height", d => d.y1 - d.y0)
-            .style("fill", "orange")
+            .style("fill", d => colorScale(d.data.category))
             .on("mousemove", (d, i) => {
                 tooltip.html(`Name: ${d.data.name}<br>Category: ${d.data.category}<br>Value: ${d.data.value}`)
                     .attr("data-value", d.data.value)
@@ -50,14 +80,62 @@ window.onload = function() {
                 tooltip.style("opacity", 0);
             });
 
-        svg.selectAll("text")
-            .data(root.leaves())
+        containers.append("text")
+            .attr("class", "tile-text")
+            .selectAll("tspan")
+            .data((d, i) => d.data.name.split(/[\s/](?=[(a-zA-Z])(?!of)(?!Duty)(?!in)(?!a\s)(?!U\s)/g).map(word => { return {text: word, 
+                                                                                width: d.x0 + (d.x1 - d.x0) / 2,
+                                                                                height: d.y0 + 10}}))
+            .enter()
+            .append("tspan")
+            .attr("x", d => d.width)
+            .attr("y", (d, i) => d.height + 10 * i)
+            .attr("dominant-baseline", "middle")
+            .attr("text-anchor", "middle")
+            .attr("inline-size", "180")
+            .text(d => d.text);
+
+
+        legend.selectAll("rect")
+            .data(categories)
+            .enter()
+            .append("rect")
+            .attr("class", "legend-item")
+            .attr("height", 20)
+            .attr("width", 20)
+            .attr("y", (d, i) => 30 + 33 * i)
+            .attr("x", 30)
+            .style("fill", d => colorScale(d))
+
+        legend.selectAll("text")
+            .data(categories)
             .enter()
             .append("text")
-            .attr("class", "tile-text")
-            .attr("x", d => d.x0 + 5)
-            .attr("y", d => d.y0 + 9)
-            .text(d => d.data.name);
+            .attr("x", 55)
+            .attr("y", (d, i) => 43 + 33 * i)
+            .text(d => d)
+            .style("font-size", "10px");
 
     });
+}
+
+
+function shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
 }
